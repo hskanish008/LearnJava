893d5c760366c259aac50bbb56d462e545939d74
Hritik <hskanish008@gmail.com>
2026-04-10 01:51:47 +0530

    c

--- src/mystring.java
+++ src/mystring.java
@@ -1,10 +1,12 @@
 public class mystring {
     public static void main(String args[])
     {
+        String c= "Hello guys";
         String firstName="Hritik";
         String lastName=" Sharma";
         String fullName=firstName+lastName;
         System.out.println(fullName);
+        System.out.println(c);
 
     }
 
