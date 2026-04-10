7af603d533404b14672556e2de28e3faee731a3e
Hritik <hskanish008@gmail.com>
2026-04-11 01:18:09 +0530

    adding student id,score,grade

--- src/mystring.java
+++ src/mystring.java
@@ -1,12 +1,23 @@
 public class mystring {
     public static void main(String args[])
     {
-        String c= "Hello guys";
-        String firstName="Hritik";
-        String lastName=" Sharma";
-        String fullName=firstName+lastName;
-        System.out.println(fullName);
-        System.out.println(c);
+//        String c= "Hello guys";
+//        String firstName="Hritik";
+//        String lastName=" Sharma";
+//        String fullName=firstName+lastName;
+//        System.out.println(fullName);
+//        System.out.println(c);
+
+        int studentID=117;
+        double score=98.77d;
+        char grade='O';
+
+        System.out.println(studentID);
+        System.out.println(score);
+        System.out.println(grade);
+
+
+
 
     }
 
