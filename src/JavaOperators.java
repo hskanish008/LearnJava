9e7e6f9f0d0f1505a132d7f389312b425d7b8576
Hritik <hskanish008@gmail.com>
2026-04-11 14:14:40 +0530

    java operators

---
+++ src/JavaOperators.java
@@ -0,0 +1,77 @@
+public class JavaOperators {
+    public static void main(String args[])
+    {
+
+
+//        int sum1=100+5;
+//        int sum2=sum1+30;
+//        int sum3=sum2+sum1;
+//        System.out.println(sum1);
+//        System.out.println(sum2);
+//        System.out.println(sum3);
+
+
+
+//       (((( --Arithmetic Operators-- ))))
+
+//        int x=10;
+//        int y=20;
+//        int z=5;
+//        System.out.println(x+y);
+//        System.out.println(x-y);
+//        System.out.println(x/y);
+//        System.out.println(x*y);
+//        System.out.println(x%y);
+//
+//        ++z;
+//        System.out.println(z);
+//        --z;
+//        System.out.println(z);
+
+
+
+//        int peopleinRoom=0;
+//        peopleinRoom++;
+//        peopleinRoom++;
+//        peopleinRoom++;
+//
+//        System.out.println(peopleinRoom);
+//        --peopleinRoom;
+//        System.out.println(peopleinRoom);
+
+
+//        ----(((( Assignment Operators ))))
+
+//        int x=10;
+//        int y=20;
+//        System.out.println(x-=y);
+//        System.out.println(x+=y);
+//
+//        System.out.println(x/=y);
+//        System.out.println(x*=y);
+//        System.out.println(x&=y);
+//        System.out.println(x|=y);
+//        System.out.println(x^=y);
+//        System.out.println(x>>=y);
+//        System.out.println(x<<=y);
+
+//        ----((((Comparison Operators))))----
+//        int x=10;
+//        int y=20;
+//        System.out.println(x<y);
+//        System.out.println(x==y);
+//        System.out.println(x>y);
+//        System.out.println(x>=y);
+
+        int age=18;
+        System.out.println(age>=18);
+
+        int passwordlength=5;
+        System.out.println(passwordlength>=8);
+        System.out.println(passwordlength<8);
+
+
+
+
+    }
+}
