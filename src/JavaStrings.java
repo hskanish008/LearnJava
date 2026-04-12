4b08da6c14350cf33d1500227c1559679a7488ba
Hritik <hskanish008@gmail.com>
2026-04-12 12:55:00 +0530

    speacial character

--- src/JavaStrings.java
+++ src/JavaStrings.java
@@ -7,7 +7,7 @@ public class JavaStrings {
         System.out.println(greeting.toUpperCase()); //toUpperCase() method converts a string to upper case letters
         System.out.println(greeting.toLowerCase()); //toLowerCase() method converts a string to lower case letters
 
-//      ---Java counts position from 0---
+//      ---index() method , Java counts position from 0---
         String stringIndex="Hii guys i am learning   java and now i am using index method to understanding strings methods ";
         System.out.println(stringIndex.indexOf("java"));
 
@@ -45,6 +45,47 @@ public class JavaStrings {
         String concat1="Hare";
         String concat2=" Krishna";
         System.out.println(concat1.concat(concat2));
+
+//        If you add two numbers ,the result will be number
+        int x=10;
+        int y=20;
+        System.out.println(x+y);
+
+//        If you add two strings,the result will be a string concatenation
+        String string1="Hare krishna";
+        String string2=" Hare Rama";
+        System.out.println(string1+string2);
+
+//        If you add a number to a string the result will be string concatenation
+        String string3="Hare Krishna ";
+        int favouriteNumber=21;
+        System.out.println(string3+favouriteNumber);
+
+
+//         ----String special Characters-----
+//        Because strings must be written within quotes, Java will misunderstand this string, and generate an error
+//        String txt = "We are the so-called "Vikings" from the north.";
+//        The solution is to avoid this problem, to use the backslash escape character
+//        The backslash (\) escape character turns special characters into string characters:
+        String txt = "We are the so called \"Vikings\" from the north.";
+            System.out.println(txt);
+
+
+        String singleComma="It\'s alright";
+        System.out.println(singleComma);
+
+//        \n	New Line
+//        \t	Tab
+//        \b	Backspace
+//        \r	Carriage Return
+//        \f	Form Feed
+
+        String specialCharacter="Hey guys \nmy name is cha muh hi \tand i am telling my \bstory about how this situation works with different people";
+        System.out.println(specialCharacter);
+//
+
+
+
 
     }
 }
