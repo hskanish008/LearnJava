effdaf03c3f394199540fe9bb33dd44d735ad2e0
Hritik <hskanish008@gmail.com>
2026-04-20 16:23:12 +0530

    Switch statement

---
+++ src/$9switch.java
@@ -0,0 +1,77 @@
+import java.util.*;
+public class $9switch {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        System.out.print("Enter the no to which day is today = ");
+        int day=sc.nextInt();
+
+//     Instead of writing many if..else statements, you can use the switch statement.
+//     Think of it like ordering food in a restaurant: If you choose number 1, you get Pizza. If you choose 2, you get a Burger. If you choose 3, you get Pasta. Otherwise, you get nothing.
+                     
+/*  switch(expression) {
+        case x:
+        code block
+        break;
+        case y:
+      code block
+        break;
+        default:
+    code block 
+*/
+
+        switch(day)
+        {
+           case 1:
+            System.out.println("Sunday");
+            break;
+           case 2:
+            System.out.println("Monday");
+            break;
+            case 3:
+            System.out.println("Tuesday");
+            break;
+            case 4:
+            System.out.println("Wednesday");
+            break;
+            case 5:
+            System.out.println("Thurday");
+            break;
+            case 6:
+            System.out.println("Friday");
+            break;
+            case 7:
+            System.out.println("Saturday");
+            break;
+            default:
+            System.out.println("There are 7 days in a week");
+        }
+
+//       A customer wants to order Coffee.
+//       The code in the editor is incomplete. Fix it by inserting the missing values so the program prints the correct order  
+
+    
+            int choice = 1;
+
+    // 1 = Coffee
+    // 2 = Tea
+
+    switch (choice) {
+      case 1:
+        System.out.println("You ordered Coffee");
+      break;
+
+      case 2:
+        System.out.println("You ordered Tea");
+      break;
+       default:
+        System.out.println("Invalid choice");
+    }
+         
+        
+        
+        
+             
+    }
+
+}
