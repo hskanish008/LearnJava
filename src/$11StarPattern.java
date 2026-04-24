ebf465842fac8c4e35f2246dc25688bd9ae90ef5
Hritik <hskanish008@gmail.com>
2026-04-24 17:11:45 +0530

    10 star pattern solve

--- src/$11StarPattern.java
+++ src/$11StarPattern.java
@@ -6,12 +6,19 @@ public class $11StarPattern {
         System.out.print("Enter the no to print : ");
         int starNum=sc.nextInt();
 
-        //      ----First pattern
-        //       * 
-        //       * * 
-        //       * * * 
-        //       * * * * 
-        //       * * * * * 
+ /*
+            ----First pattern
+
+
+
+              * 
+              * * 
+              * * * 
+              * * * * 
+              * * * * * 
+
+ */        
+
         // for(int row=1;row<=starNum;row++)
         // {
         //     for(int col=1;col<=row;col++)
@@ -21,8 +28,9 @@ public class $11StarPattern {
         //     System.out.println();
         // }
 
+
 /*
-        ----Second Pattern----
+                   ----Second Pattern----
 
 * * * * * 
 * * * * 
@@ -48,29 +56,189 @@ public class $11StarPattern {
                 // }
 
                 
-                // ----Third Pattern----
-/*
+ /*                ----Third Pattern----
+
+
     *
    **
   ***
  ****
 ***** 
+
+
 */               
 
-                for(int row=1;row<=starNum;row++)
-                {
-                    for(int col=starNum-1;col>=row;col--)
-                    {
-                        System.out.print(" ");
-                    }
-                    for(int pat=1;pat<=row;pat++)
-                    {
-                     System.out.print("*");
+                // for(int row=1;row<=starNum;row++)
+                // {
+                //     for(int col=starNum-1;col>=row;col--)
+                //     {
+                //         System.out.print(" ");
+                //     }
+                //     for(int pat=1;pat<=row;pat++)
+                //     {
+                //      System.out.print("*");
 
-                    }
+                //     }
                     
-                    System.out.println();
-                }
+                //     System.out.println();
+                // }
+
+
+/*              ----Fourth Pattern----
+
+
+ ****
+  ***
+   **
+    *
+
+ */
+
+
+                // for(int row=1;row<=starNum;row++)
+                // {
+                //     for(int col=2;col<=row;col++)
+                //     {
+                //         System.out.print(" ");
+                //     }
+                //     for(int pat=starNum;pat>=row;pat--)
+                //     {
+                //         System.out.print("*");
+                //     }
+                //     System.out.println();
+                // }
+
+
+/*              ----Fifth Pattern----
+ 
+    * 
+   * * 
+  * * * 
+ * * * * 
+* * * * * 
+
+     
+
+*/        
+
+                // for(int row=1;row<=starNum;row++)
+                // {
+                //     for(int col=row+1;col<=starNum;col++)
+                //     {
+                //         System.out.print(" ");
+                //     }
+                //     for(int pat=1;pat<=row;pat++)
+                //     {
+                //         System.out.print("* ");
+                //     }
+                //     System.out.println();
+                // }
+                
+              
+
+/*              ----Sixth Pattern----
+
+      ****
+      ****
+      ****
+      ****    
+
+*/  
+            //     for(int row=1;row<=starNum;row++)
+            //   {
+            //    for(int col=1;col<=starNum;col++)
+            //    {
+            //    System.out.print("*");
+            //    }
+            //    System.out.println();
+            //   }
+
+
+
+/*              ----Seven Pattern----
+
+       1
+       12
+       123
+       1234
+       12345   
+
+*/  
+        // for(int row=1;row<=starNum;row++)
+        // {
+        //    for(int col=1;col<=row;col++)
+        //    {
+        //    System.out.print(col);
+        //    }
+        //    System.out.println();
+        // }
+
+
+/*              ----Eight Pattern----
+ 
+  1
+  22
+  333
+  4444
+  55555 
+          
+
+*/
+            // for(int row=1;row<=starNum;row++)
+            // {
+            //  for(int col=1;col<=row;col++)
+            // {
+            //  System.out.print(row);
+            // }
+            //  System.out.println();
+            // }
+
+/*              ----Nine Pattern----
+  12345
+  1234
+  123
+  12
+  1 
+               
+           learn this will help in future no-row+1
+
+*/
+        
+            //   for(int row=1;row<=starNum;row++)
+            //    {
+            //     for(int col=1;col<=starNum-row+1;col++)
+            //      {
+                    
+            //       System.out.print(col); 
+            //      }
+                
+            //     System.out.println();
+            //    }
+
+/*              ----Tenth Pattern----
+  
+    *
+   ***
+  *****
+ *******
+*********              
+            
+*/
+             for(int row=1;row<=starNum;row++)
+            {
+             for(int col=1;col<=starNum-row;col++)
+             {
+             System.out.print(" ");
+             }
+             for(int pat=1;pat<=2*row-1;pat++)
+             {
+                System.out.print("*");
+             }
+             System.out.println();
+            }
+
+
+
 
     }
 
