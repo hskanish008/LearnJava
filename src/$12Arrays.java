96eaeb7bc20cf793c1cefe152fe1234a5da0861b
Hritik <hskanish008@gmail.com>
2026-04-28 21:22:42 +0530

    arrays with for each

--- src/$12Arrays.java
+++ src/$12Arrays.java
@@ -1,6 +1,85 @@
 public class $12Arrays {
     public static void main(String[] args)
     {
+
+
+/*
+
+                       Java Arrays
+Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+To declare an array, define the variable type with square brackets [ ] :
+
+We have now declared a variable that holds an array of strings. To insert values to it, you can place the values in a comma-separated list, inside curly braces { }:
+
+Access the Elements of an Array
+You can access an array element by referring to the index number.
+
+This statement accesses the value of the first element in cars:
+
+Array indexes start with 0: [0] is the first element. [1] is the second element, etc.
+
+Think of an array as numbered boxes, where each box stores an element:
+
+Index	Element
+0	    Volvo
+1	    BMW
+2	    Ford
+3	    Mazda
+
+Change an Array Element
+To change the value of a specific element, refer to the index number:
+
+cars[0] = "Opel";
+
+Array Length
+To find out how many elements an array has, use the length property:
+
+Example
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+System.out.println(cars.length);
+// Outputs 4
+
+
+The new Keyword
+You can also create an array by specifying its size with new. This makes an empty array with space for a fixed number of elements, which you can fill later:
+
+Example
+String[] cars = new String[4]; // size is 4
+
+cars[0] = "Volvo";
+cars[1] = "BMW";
+cars[2] = "Ford";
+cars[3] = "Mazda";
+
+System.out.println(cars[0]); // Outputs Volvo
+
+
+However, if you already know the values, you don't need to write new. Both of these create the same array:
+
+Example
+// With new
+String[] cars = new String[] {"Volvo", "BMW", "Ford", "Mazda"};
+
+// Shortcut (most common)
+String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+
+
+Note: You cannot write new String[4] {"Volvo", "BMW", "Ford", "Mazda"}.
+
+In Java, when using new, you either:
+
+Use new String[4] to create an empty array with 4 slots, and then fill them later
+Or use new String[] {"Volvo", "BMW", "Ford", "Mazda"} (without specifying the number of elements) to create the array and assign values at the same time
+Tip: The shortcut syntax is most often used when the values are known at the start. Use new with a size when you want to create an empty array and fill it later.
+
+
+
+*/
+
+
+
         String[] cars={"Mercedes","Bmw","Lamborgini","Bentely"};
 
     //  How to print with one element in a simple way.
@@ -15,10 +94,88 @@ public class $12Arrays {
     
     // how to print string with a integer way.
 
-        for(int num=0;num<4;num++)
+/*
+
+Loop Through an Array
+You can loop through the array elements with the for loop, and use the length property to specify how many times the loop should run.
+
+This example creates an array of strings and then uses a for loop to print each element, one by one:
+
+ */
+              
+         
+
+        for(int num=0;num<cars.length;num++)
         {
             System.out.println("The total no of elements with a number way :"+cars[num]);
         }
+
+
+
+// Here is a similar example with numbers. We create an array of integers and use a for loop to print each value:
+
+
+     int[] numbers={10,20,30,40,50};
+    
+    for(int n1=0;n1<numbers.length;n1++)
+    {
+        System.out.println(numbers[n1]);
+    }
+    
+
+/*
+                    Calculate the Sum of Elements
+Now that you know how to work with arrays and loops, let's use them together to calculate the sum of all elements in an array:
+ */
+
+         int[] arrSum={1,2,3,4,5,6,7,8,9,10};
+    int sum=0;
+    
+    for(int arrNum=0;arrNum<arrSum.length;arrNum++)
+    {
+        sum+=arrSum[arrNum];
+        
+    }
+    System.out.println("The sum of all array elements are = "+sum);
+
+
+
+/*
+
+Loop Through an Array with For-Each
+There is also a "for-each" loop, which is used exclusively to loop through elements in an array (or other data structures):
+
+Syntax
+for (type variable : arrayname) 
+{
+  // code block to be executed
+}
+The colon (:) is read as "in". So you can read the loop as: "for each variable in array".
+
+The following example uses a for-each loop to print all elements in the cars array:
+
+
+ */
+
+
+    //    String[] carrs = {"Volvo", "BMW", "Ford", "Mazda"};
+
+    //    for (String car : carrs)
+    //     {
+    //       System.out.println(car);
+    //     }
+
+
+//     int[] num={1,2,3,4,5,6,7,8,9,10};
+//    for(int n : num)
+//    {
+//        System.out.println(n);
+//    }
+
+    
+
+
+
 
     }
 
