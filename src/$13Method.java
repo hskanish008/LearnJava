411bf968a486a91fe25ed59ecb8638c66fd908a4
Hritik <hskanish008@gmail.com>
2026-05-01 22:01:13 +0530

    java methods

---
+++ src/$13Method.java
@@ -0,0 +1,18 @@
+public class $13Method {
+
+    static void helloHritik()
+    {
+        System.out.println("Hii hritik");
+    }
+
+    static void helloSandeep()
+    {
+        System.out.println("Hii Sandeep");
+    }
+
+    public static void main(String args[])
+        {
+            helloHritik();
+        }
+    
+}
